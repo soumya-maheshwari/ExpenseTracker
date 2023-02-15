6fb648ed24ebd8652caf7f698c054f1c74b4909c
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-02-16 01:25:26 +0530

    updated spent

--- src/Components/Spent.js
+++ src/Components/Spent.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { AppContext } from "../Context/AppContext";
+import { useContext } from "react";
 const Spent = () => {
+  const { expenses } = useContext(AppContext);
+  // Using the reduce function to get a total of all the costs and assigning this to a variable
+  const totalExpense = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
   return (
     <div className="alert alert-primary">
-      <span>Spent so far:</span>
+      <span>Spent so far: {totalExpense}</span>
     </div>
   );
 };
